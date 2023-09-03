578327206eecac8886dfffee8af61c2acbab47f7
Artem <3497134@mail.ru>
2023-09-03 17:44:36 +0300

    add routs and other changes

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::middleware(['authorized'])->group(function () {
     Route::get('/messages/create', [\App\Http\Controllers\MessageController::class, 'create']);
     Route::post('/messages/send', [\App\Http\Controllers\MessageController::class, 'send'])->name('messages.send');
 
+    Route::get('/notifications', [NotificationsController::class, 'index'])->name('notifications.index');
+    Route::post('/notifications/mark-as-read/{id}', [NotificationsController::class, 'markAsRead'])->name('notifications.markAsRead');
+
 });
 Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 
