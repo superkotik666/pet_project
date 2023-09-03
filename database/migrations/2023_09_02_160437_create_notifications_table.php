f3510d01c024871bfce6ae351ba6a56a3c4e0a0d
Artem <3497134@mail.ru>
2023-09-03 17:34:18 +0300

    add changes in migration

--- database/migrations/2023_09_02_160437_create_notifications_table.php
+++ database/migrations/2023_09_02_160437_create_notifications_table.php
@@ -4,27 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateNotificationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
             $table->text('text');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('notifications');
     }
-};
+}
