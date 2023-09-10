86d793c65944c94b24017df63066f7cdc83ef788
Artem <3497134@mail.ru>
2023-09-10 23:56:56 +0300

    add  forgot password password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ForgotPassword;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -69,6 +71,31 @@ class AuthController extends Controller
     public function notauth()
     {
         return view('auth.notauth');
+    }
+
+
+    public function showForgotForm()
+    {
+        return view("auth.forgot");
+    }
+
+
+    public function forgot(Request $request){
+
+        $data = $request->validate(
+            ["email" => ["required", "email", "string", "exists:users"],]);
+
+        $user = User::where(["email" => $data["email"]])->first(); // выбираем пользователя, у которого имэйл совпадает с введенным в форму
+
+        $password = uniqid(); // мы взяли уникальную последовательнось символов
+        $user->password =bcrypt($password); //захешировали эту последовательность
+        $user->save(); // и присвоили данное значение выбранному юзеру, выполнив замену пороля в таблице, сохранив результат
+
+        Mail::to($user)->send(new ForgotPassword($password)); // отправляем выбранному юзеру новый пароль на имэйл
+
+        return redirect(route('home'));
+
+
     }
 
 }
