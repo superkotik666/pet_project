d43be84a574fbfc5d664693dc6d0486a76c52d00
Artem <3497134@mail.ru>
2023-09-10 23:58:26 +0300

    create forgot password class

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Events\CommentCreated;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +19,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        CommentCreated::class =>[]
     ];
 
     /**
@@ -28,9 +30,7 @@ class EventServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     */
+
     public function shouldDiscoverEvents(): bool
     {
         return false;
