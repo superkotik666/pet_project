11fb455922020811f83b004fe1b36afd73bca4e6
Artem <3497134@mail.ru>
2023-09-13 15:41:47 +0300

    create RegisterFormRequest.php

---
+++ app/Http/Requests/RegisterFormRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            "name" => ["required", "string"],
+            "email" => ["required", "email", "string", "unique:users,email"],
+            "password" => ["required", "confirmed"],
+        ];
+    }
+}
