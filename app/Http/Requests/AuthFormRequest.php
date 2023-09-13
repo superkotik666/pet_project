3014eb485dcb5e91a84bf46e3e75b29eaa90b741
Artem <3497134@mail.ru>
2023-09-13 15:04:01 +0300

    create AuthFormRequest

---
+++ app/Http/Requests/AuthFormRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            "email" => ["required", "email", "string"],
+            "password" => ["required"],
+        ];
+    }
+}
